568b9496dd3233d120e925ad3f13f0787c20eb90
Oleksandra Lishchenko <UL0279937@edu.uni.lodz.pl>
2024-11-15 21:00:05 +0100

    First Commit

--- app/src/main/java/com/example/assignmnetnew4/LogInActivity.kt
+++ app/src/main/java/com/example/assignmnetnew4/LogInActivity.kt
@@ -1,21 +1,20 @@
 package com.example.assignmnetnew4
 
-import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import androidx.appcompat.app.AppCompatActivity
-
 
 class LogInActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.account_log_in)
 
-        // Handle "Register Now" click to navigate to SignUpActivity
         findViewById<TextView>(R.id.RegisterNowText)?.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+            setContentView(R.layout.account_sign_up)
+
+            findViewById<TextView>(R.id.LogInText)?.setOnClickListener {
+                setContentView(R.layout.account_log_in)
+            }
         }
     }
 }
--- app/src/main/java/com/example/assignmnetnew4/MainActivity.kt
+++ app/src/main/java/com/example/assignmnetnew4/MainActivity.kt
@@ -5,16 +5,13 @@ import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.account_log_in)
 
         findViewById<TextView>(R.id.RegisterNowText)?.setOnClickListener {
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, SignUpActivity::class.java))
             finish()
         }
     }
